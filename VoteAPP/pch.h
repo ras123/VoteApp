b61073cacdec803a377a4b3e7190c9fcb91c481a
Michael <miczbeetnoff@hotmail.com>
2012-11-10 13:39:12 -0800

    Blank screen, with LOGO

---
+++ VoteAPP/pch.h
@@ -0,0 +1,13 @@
+﻿//
+// pch.h
+// Header for standard system include files.
+//
+
+#pragma once
+
+// Added these files - MZ
+#include <iostream>
+#include <iomanip>
+
+#include <collection.h>
+#include "App.xaml.h"
